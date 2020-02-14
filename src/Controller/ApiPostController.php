389bffbecc260d8e687c7ff4115123687bf9172e
Julien BASQUIN <jbasquin@iorga.com>
2020-02-14 18:32:47 +0100

    initial commit

---
+++ src/Controller/ApiPostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ApiPostController extends AbstractController
+{
+    /**
+     * @Route("/api/post", name="api_post_index", methods={"GET"})
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository)
+    {
+        // le precessus de serialisation fais les deux en meme temps ->
+        //normalisation des donnes = transformer des objets vers tableau assosiatif
+        //encodage = transformer tableau assosiatif vers text
+        return $this->json($postRepository->findAll(), 200, [], ['groups' => 'post:read']);
+    }
+
+    /**
+     * @Route("/api/post", name="api_post_store", methods={"POST"})
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    {
+        $jsonRecu = $request->getContent();
+        try
+        {
+            $post = $serializer->deserialize($jsonRecu, Post::class, 'json');
+            $post->setCreatedAt(new \DateTime());
+            $errors = $validator->validate($post);
+            if (count($errors) > 0){
+                return $this->json($errors, 400);
+            }
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->json($post, 201, [], ['groups' => 'post:read']);
+        } catch (NotEncodableValueException $e){
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
